f7536b49af0498c0b484527d1b7b9c437b787ad6
Masaaki Goshima <goccy54@gmail.com>
2025-08-04 22:03:23 +0900

    wip

--- cmd/scenarigo/main.go
+++ cmd/scenarigo/main.go
@@ -8,12 +8,16 @@ import (
 	"os/signal"
 
 	"github.com/fatih/color"
+	"github.com/mattn/go-isatty"
 
 	"github.com/scenarigo/scenarigo/cmd/scenarigo/cmd"
 )
 
 func main() {
 	fmt.Println("color.NoColor", color.NoColor)
+	fmt.Println("TERM", os.Getenv("TERM"))
+	fmt.Println("isatty.IsTerminal(os.Stdout.Fd())", isatty.IsTerminal(os.Stdout.Fd()))
+	fmt.Println("isatty.IsCygwinTerminal(os.Stdout.Fd())", isatty.IsCygwinTerminal(os.Stdout.Fd()))
 	if err := run(); err != nil {
 		if errors.Is(err, cmd.ErrTestFailed) {
 			os.Exit(10)
